f245dcc8910b9e4580ee287453f4e48d2836174d
Hugo Caron <hca443@gmail.com>
2024-09-06 16:22:43 +0200

    switch to ztnet API

--- zoneit/zt_lease.py
+++ zoneit/zt_lease.py
@@ -26,9 +26,21 @@ async def get_zerotier_clients(api_token: str, network_id: str):
     return data
 
 
-async def zt_dhcp_lease(zt_token, zt_network):
+async def get_ztnet_clients(api_token: str, network_id: str):
+    headers = {"x-ztnet-auth": f"{api_token}"}
+    url = f"https://ztnet.mazenet.org/api/v1/network/{network_id}/member/"
+
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, headers=headers)
+        response.raise_for_status()
+        data = response.json()
+
+    return data
+
+
+async def zt_dhcp_lease_original(zt_token, zt_network):
     # Format the output as per your requirements
-    data = await get_zerotier_clients(zt_token, zt_network)
+    data = await get_ztnet_clients(zt_token, zt_network)
     clients = []
     for device in data:
         ips = device["config"]["ipAssignments"]
@@ -37,6 +49,24 @@ async def zt_dhcp_lease(zt_token, zt_network):
             "hostname": device.get("name", None),
             "ip_address": ip,
             "id": device["config"]["address"],
+        }
+        if client["hostname"]:
+            clients.append(client)
+
+    return clients
+
+
+async def zt_dhcp_lease(zt_token, zt_network):
+    # Format the output as per your requirements
+    data = await get_ztnet_clients(zt_token, zt_network)
+    clients = []
+    for device in data:
+        ips = device["ipAssignments"]
+        ip = ips[0] if len(ips) else None
+        client = {
+            "hostname": device.get("name", None),
+            "ip_address": ip,
+            "id": device["address"],
         }
         if client["hostname"]:
             clients.append(client)
